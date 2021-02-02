e26fd342feb2269be274399a3d991a5d2e01057a
Madhav Anand <madhav1004@icloud.com>
2021-02-02 18:04:51 +0530

    customized the portfolio.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,28 +7,37 @@ import Resume from "../Components/Resume";
 import Contact from "../Components/Contact";
 import Testimonials from "../Components/Testimonials";
 import Portfolio from "../Components/Portfolio";
+import Head from "next/head";
 
 const App = () => {
   const [resumeData, setResumeData] = useState({});
 
   useEffect(() => {
     fetch("/resumeData.json")
-        .then((res) => res.json())
-        .then((data) => {
-          setResumeData(data);
-        });
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+      });
   }, []);
 
   return (
-      <div className="App">
-        <Header data={resumeData.main} />
-        <About data={resumeData.main} />
-        <Resume data={resumeData.resume} />
-        <Portfolio data={resumeData.portfolio} />
-        <Testimonials data={resumeData.testimonials} />
-        <Contact data={resumeData.main} />
-        <Footer data={resumeData.main} />
-      </div>
+    <div className="App">
+      <Head>
+        <title>Angelo's Resume: The way of everything</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;800;900&family=Montserrat:wght@400;500;600;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Header data={resumeData.main} />
+      <About data={resumeData.main} />
+      <Resume data={resumeData.resume} />
+      <Portfolio data={resumeData.portfolio} />
+      <Testimonials data={resumeData.testimonials} />
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
+    </div>
   );
 };
 
