07d14d15cc69887bc05175d0b533d967446d4b82
Madhav Anand <madhav1004@icloud.com>
2021-02-02 16:55:14 +0530

    fixed the portfolio for next js.

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import '../../styles/globals.css'
+import "../../styles/App.css";
+import "../../styles/default.css";
+import "../../styles/fonts.css"
+import "../../styles/layout.css"
+import "../../styles/media-queries.css"
+import "../../styles/magnific-popup.css"
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
